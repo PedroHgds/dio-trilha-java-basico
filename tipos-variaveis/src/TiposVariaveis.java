c7abc02b748eb7e23eccc478f7ce04a610df658e
PedroHgds <phgds741@hotmail.com>
2023-03-08 14:31:35 -0300

    estudos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,32 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudar classe String
+
+        String meuNome= "Pedro";
+
+
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal= 2;
+        short numeroCurto2 = (short) numeroNormal;
+
+        
+        int numero =5;
+        
+        System.out.println(numero);
+            final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
+        System.out.println(meuNome);
+
+        System.out.println(salarioMinimo);
+
+        System.out.println(numeroNormal);
+
+        System.out.println(numeroCurto2);
+
+        System.out.println(numeroCurto);
+
     }
 }
