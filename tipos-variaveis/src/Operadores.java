747d5174b3d2c24f04b6ed17c41e657a4b475081
PedroHgds <phgds741@hotmail.com>
2023-03-08 15:02:02 -0300

    operadores prt2

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,49 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+
+        String nome = "Pedro";
+        System.out.println(nome);
+        int idade = 23;
+        System.out.println(idade);
+        double peso = 70.5;
+        System.out.println(peso);
+        char sexo = 'M';
+        System.out.println(sexo);
+        boolean doadorOgao = false;
+        System.out.println(doadorOgao);
+
+        Date dataNascimento = new Date();
+
+        double soma = 10.5 + 15.7;
+        System.out.println(soma);
+        int subtracao = 113 - 25;
+        System.out.println(subtracao);
+        int multiplicao = 20*7;
+        System.out.println(multiplicao);
+        int divisao = 15 / 3;
+        System.out.println(divisao);
+        int modulo = 18 % 3;
+        System.out.println(modulo);
+        double resultado = (10*7) + (20/4);
+        System.out.println(resultado);
+
+
+        String nomeCompleto = "LINGUAGEM " + " JAVA";
+
+        String concatenacao = "?";
+            System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+            System.out.println(concatenacao);
+        
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+        
+
+
+    
+}
