72246aeb8d1206dadbabbb43767d40c1b0878707
PedroHgds <phgds741@hotmail.com>
2023-03-09 08:37:35 -0300

    adicionando explos de escopo

--- tipos-variaveis/src/Escopo_Exemplo.java
+++ tipos-variaveis/src/Escopo_Exemplo.java
@@ -15,6 +15,15 @@ public void imprimirSaldo(){
 
     //System.out.println(novoSaldo);{}
 }
-
+public double calcularDividaExponencial(){
+    //variavel local de método
+    double valorParcela = 50.0;
+    double valorMontante = 0.0; 
+    
+        for(int x=1; x<=5; x++){
+            double valorCalculado = valorParcela *x;
+            valorMontante = valorMontante + valorCalculado;
+    }
+}
 
 }
