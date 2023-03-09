b524d5f3a1059c966986032262b0faaf1594944c
PedroHgds <phgds741@hotmail.com>
2023-03-09 17:24:36 -0300

    ajustes finais

---
+++ tipos-variaveis/src/teste.java
@@ -0,0 +1,7 @@
+public class teste {
+    public static void main(String[] args) throws Exception {
+        int numero1 = 1;
+        String numero2 = "2";
+        System.out.println(numero1+numero2); 
+      }
+}
