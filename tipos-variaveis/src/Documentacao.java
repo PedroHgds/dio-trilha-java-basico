7c64369216a7d088451436a9c0886e908f8a3337
PedroHgds <phgds741@hotmail.com>
2023-03-09 17:09:32 -0300

    estudos sobre terminal e agumentos

---
+++ tipos-variaveis/src/Documentacao.java
@@ -0,0 +1,24 @@
+// https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+
+/**
+* <h1>Calculadora</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Pedro
+* @version 1.0
+* @since   09/03/2023
+*/
+public class Calculadora {
+    /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a soma dos dois números.
+   */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+}
